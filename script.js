fb2bcf4b238d2eaef8645f65a59f9b2dc3f700be
Jisun696 <istiakkashem35@gmail.com>
2024-03-08 00:05:40 +0600

    solved bugs in addTask function & styled the tasks that'll be added

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ const generateCalender = () => {
   for (let day = 1; day <= totalDays; day++) {
     const daySquare = document.createElement('div');
     daySquare.className = 'calender-day';
+    daySquare.classList.add('flex', 'flex-col', 'items-center', 'bg-white', 'text-[#c4c4c4]', 'p-4', 'rounded-lg', 'min-h-16', 'shadow-lg');
     daySquare.textContent = day;
     daySquare.id = `day-${day}`;
     calender.appendChild(daySquare);
@@ -55,11 +56,12 @@ function addTask() {
 
   if (taskDesc && !isNaN(taskDate.getDate())) {
     const calenderDays = document.getElementById('calender').children;
-    for (let i = 0; i < calenderDays; i++) {
+    for (let i = 0; i < calenderDays.length; i++) {
       const day = calenderDays[i];
-      if (parseInt(day.textContent) === taskDate.getDay()) {
+      if (parseInt(day.textContent) === taskDate.getDate()) {
         const taskElement = document.createElement('div');
         taskElement.className = 'task';
+        taskElement.classList.add('bg-black');
         taskElement.textContent = taskDesc;
 
         taskElement.addEventListener('contextmenu', function (event) {
